e4cdb6d379a2488d1845a6fee56fc5c36e4e7fb9
Haorui Wu <srxiaoj@gmail.com>
2016-08-09 00:54:29 -0400

    Changed tiversa background to blue, changed search_param to search_param_multi

--- assets/js/searchBy.js
+++ assets/js/searchBy.js
@@ -2,12 +2,12 @@
  * Created by zhangchuhui on 8/1/16.
  */
 $(document).ready(function(e){
-    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+    $('.search-panel-multi .dropdown-menu').find('a').click(function(e) {
         e.preventDefault();
         var param = $(this).attr("href").replace("#","");
         var concept = $(this).text();
-        $('.search-panel span#search_concept').text(concept);
-        $('.input-group #search_param').val(param);
+        $('.search-panel-multi span#search_concept_multi').text(concept);
+        $('.input-group #search_param_multi').val(param);
     });
 });
 
