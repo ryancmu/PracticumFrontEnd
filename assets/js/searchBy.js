0b727075c8fd199e877667f0f8c3a39ad6a76a2b
Haorui Wu <srxiaoj@gmail.com>
2016-08-12 01:09:31 -0400

    Changed Multi Search Box add and delete function

--- assets/js/searchBy.js
+++ assets/js/searchBy.js
@@ -2,12 +2,41 @@
  * Created by zhangchuhui on 8/1/16.
  */
 $(document).ready(function(e){
-    $('.search-panel-multi .dropdown-menu').find('a').click(function(e) {
-        e.preventDefault();
-        var param = $(this).attr("href").replace("#","");
-        var concept = $(this).text();
-        $('.search-panel-multi span#search_concept_multi').text(concept);
-        $('.input-group #search_param_multi').val(param);
+    var $search_panels = $('.multiSearch');
+    $search_panels.each(function() {
+        addClickEventListner.call(this);
     });
+
+    function add() {
+        var $clone = $(".multiSearch:first").clone(false);
+        addClickEventListner.call($clone);
+        $clone.appendTo("#wrapper");
+    }
+
+    function remove(that) {
+        that.remove();
+    }
+
+    function addClickEventListner() {
+        var that = this;
+        
+        var $dropdown_options = $(this).find(".dropdown-menu a");
+        var $dropdown_button_text = $(this).find(".dropdown_button_text");
+        var $hidden_input = $(this).find('.input-group #search_param_multi');
+        var $add_button = $(this).find("button.add_button");
+        var $remove_button = $(this).find("button.remove_button");
+
+        $add_button.click(add);
+        $remove_button.click(function() {
+            remove(that);
+        });
+
+        $dropdown_options.click(function(e) {
+            e.preventDefault();
+            var param = $(this).attr("href").replace("#","");
+            $hidden_input.val(param);
+            $dropdown_button_text.text($(this).text());
+        });
+    }
 });
 
